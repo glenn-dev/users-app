a21d93fcd2f8cd6bb99ba5e01051c8fe045fa1e4
glenn-dev <glenn.marcano90@gmail.com>
2020-05-08 11:26:14 -0400

    User interface created, user.component modified

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/users.service';
+import { User } from '../../interfaces/users';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html'
 })
 export class UsersComponent implements OnInit {
-  users: any
+  users: User
   constructor(private userServices: UserService) { }
 
   ngOnInit(){
