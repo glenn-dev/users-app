eb894bff50747d2d74d3fa1e1caec042dae676be
glenn-dev <glenn.marcano90@gmail.com>
2020-05-14 21:36:42 -0400

    Modal component fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './routing/app-routing.module';
-import { AppComponent } from './app.component';
-
-import { UsersComponent } from './components/users/users.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ModalComponent } from './components/modal/modal.component';
-
 // Firebase
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from "@angular/fire/auth";
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+// Auth service
 import { SignInComponent } from './components/auth/sign-in/sign-in.component';
 import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
 import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './components/auth/verify-email/verify-email.component';
-// Auth service
 import { AuthService } from "./services/auth.service";
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
+// App Components
+import { AppComponent } from './app.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { UsersComponent } from './components/users/users.component';
+import { ModalComponent } from './components/modal/modal.component';
+
+
 
 @NgModule({
   declarations: [
